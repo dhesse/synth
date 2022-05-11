32599bde1d05c96c6efde629e521acf643724ce8
Dirk Hesse <dirk.hesse@posteo.net>
2022-05-11 15:33:22 +0200

    Adding config class.

--- synth.py
+++ synth.py
@@ -5,20 +5,60 @@ from typing import List
 from itertools import cycle
 import soundfile as sf
 
-@dataclass
-class Oscillator:
 
-    frequency: int
-    sample_rate: int = 44100
-    amplitude: float = 1
+@dataclass
+class Sound:
+
+    sample_rate: int
 
     def get(self, start_frame: int, frames: int) -> numpy.ndarray:
         raise NotImplementedError()
+
+    def __add__(self, other):
+        return CombinedSound(self, other)
+
+@dataclass
+class CombinedSound(Sound):
+    
+    first: Sound
+    second: Sound
+
+    def get(self, start_frame: int, frames: int) -> numpy.ndarray:
+        return self.first.get(start_frame, frames) + self.second.get(start_frame, frames)
+
+
+@dataclass
+class Oscillator(Sound):
+
+    frequency: int
+    amplitude: float = 1
+
+    
+
+@dataclass
+class Echo:
+
+    sound: Oscillator
+    delay: int
+    decay: float
+    frames: numpy.ndarray = field(init=False)
+
+    def __post_init__(self):
+        self.frames = numpy.zeros((int(self.sound.sample_rate*self.delay), 1))
+
+    def get(self, start_frame:int, frames: int) -> numpy.ndarray:
+        res = self.sound.get(start_frame, frames)
+        res += self.frames[:frames,:]
+        self.frames[:frames,:] = 0#*= self.decay
+        self.frames[:frames,:] += self.decay*res
+        self.frames = numpy.roll(self.frames, -frames)
+        return res
 
 @dataclass
 class WaveGen:
 
     samples: numpy.ndarray
+    sample_rate: int = 44100
 
     def get(self, start_frame: int, frames: int) -> numpy.ndarray:
         high = start_frame + frames
@@ -115,9 +155,9 @@ def n_to_freq(n: int) -> float:
     return 2**((n - 49)/12)*440
 
 PETER_GUNN = cycle([
-    Note([PulseGen(n_to_freq(n), amplitude=0.1),
-          ToothGen(n_to_freq(n), amplitude=0.5),
-          NoiseGen(0, amplitude=0.07)])
+    Note([PulseGen(44100, n_to_freq(n), amplitude=0.1),
+          ToothGen(44100, n_to_freq(n), amplitude=0.5),
+          NoiseGen(44100, 0, amplitude=0.07)])
     for n in [21, 21, 23, 21, 24, 21, 26, 25]
 ])
 
@@ -128,30 +168,50 @@ SAMPLES = {"K": "/Users/dirkhesse/code/synth/K/428__tictacshutup__prac-kick.wav"
 
 BOOM_CHAH = cycle([WaveGen(sf.read(SAMPLES[i])[0]) for i in "KHSH"])
 
+@dataclass
+class Config:
+    bpm: int
+    sample_rate: int
+    bar_length: int
+    note_resolution: int
+
+    @property
+    def bps(self) -> float:
+        return self.bpm / 60
+
+    @property
+    def fpn(self) -> float:
+        """Frames per note"""
+        return int(self.sample_rate / self.bps
+                   / self.note_resolution * self.bar_length)
+    
+
 if __name__ == "__main__":
 
-    sounds = zip(BOOM_CHAH)#, PETER_GUNN)
+    sounds = zip(BOOM_CHAH, PETER_GUNN)
     notes = next(sounds)
-    bpm = 100
-    bps = bpm / 60
-    sample_rate = 44100
-    bar_length = 4
-    note_resolution = 8 # 8th notes
-    fpn = int(sample_rate / bps / note_resolution * bar_length)
+    cfg = Config(bpm = 100, sample_rate = 44100,
+                 bar_length = 4, note_resolution = 8)
+    fpn = cfg.fpn
     idx = 0
+    #echo = Echo(note, 0.5, 0.12)
     def callback(outdata, frames, time, status):
         global idx
         global note
         global notes
+        global echo
         start_idx = 0
         while idx + frames > fpn:
             frames_left_on_note = fpn - idx
+            #data = echo.get(idx, frames_left_on_note)
             data = sum(note.get(idx, frames_left_on_note) for note in notes)
             outdata[start_idx:start_idx + frames_left_on_note] = data
             start_idx += frames_left_on_note
+            #echo.sound = next(sounds)[0]
             notes = next(sounds)
             frames -= frames_left_on_note
             idx = 0
+        #data = echo.get(idx, frames)
         data = sum(note.get(idx, frames) for note in notes)
         outdata[start_idx:] = data
         idx += frames
